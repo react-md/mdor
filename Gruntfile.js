bcb9116cd0676c2849404cb9543fa243a3e7db70
Tom Wu <tom76kimo@yahoo-inc.com>
2015-07-08 15:32:26 +0800

    add webpack and es6 feature

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        webpack: {
+            main: {
+                entry: './index.js',
+                output: {
+                    path: __dirname,
+                    filename: 'bundle.js'
+                },
+                stats: {
+                    colors: true,
+                    module: true
+                },
+                module: {
+                    loaders: [
+                        {test: /\.css$/, loader: 'style!css'},
+                        {
+                            test: /\.jsx?$/,
+                            exclude: /(node_modules|bower_components)/,
+                            loader: 'babel'
+                        }
+                    ]
+                },
+                watch: false,
+                keepalive: false
+            },
+            watch: {
+                entry: './index.js',
+                output: {
+                    path: __dirname,
+                    filename: 'bundle.js'
+                },
+                stats: {
+                    colors: true,
+                    module: true
+                },
+                module: {
+                    loaders: [
+                        {test: /\.css$/, loader: 'style!css'},
+                        {
+                            test: /\.jsx?$/,
+                            exclude: /(node_modules|bower_components)/,
+                            loader: 'babel'
+                        }
+                    ]
+                },
+                watch: true,
+                keepalive: true
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-webpack');
+    grunt.registerTask('default', ['webpack:main']);
+    grunt.registerTask('watch', ['webpack:watch']);
+};
