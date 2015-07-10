3656ba9e5edf2e11a4fbcbb6358bf9219c1e51ab
Jason Huang <cjhuang@yahoo-inc.com>
2015-07-10 22:13:14 +0800

    restyle index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Button from './src/components/Button';
 import Slider from './src/components/Slider';
 import Tooltip from './src/components/Tooltip';
+
 export default {
     Button: Button,
     Slider: Slider,
