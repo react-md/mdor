f252ffadf674ef4ba2c5b4eec6283b50144ada70
Jason Huang <cjhuang@yahoo-inc.com>
2015-07-10 22:12:17 +0800

    restyle Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 class Button extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  render() {
+    return (
+      <button className="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+        <i className="material-icons">add</i>
+      </button>
+    );
+  }
 
-    render() {
-        return (
-            <button className="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
-              <i className="material-icons">add</i>
-            </button>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 }
 
 export default Button;
